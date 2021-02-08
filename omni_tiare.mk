cd2d892fb721d92fd77bd1c784bef4c0a0fd4970
Aryan Sinha <sinha.aryan03@gmail.com>
2021-02-08 16:35:12 +0530

    tiare: PBRP-ify && Update Prebuilts
    Signed-off-by: Aryan Sinha <sinha.aryan03@gmail.com>

--- omni_tiare.mk
+++ omni_tiare.mk
@@ -15,11 +15,11 @@
 #
 
 # Specify phone tech before including full_phone
-$(call inherit-product, vendor/omni/config/gsm.mk)
+# $(call inherit-product, vendor/omni/config/gsm.mk)
 
 $(call inherit-product, device/xiaomi/tiare/full_tiare.mk)
 
-# Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+# Inherit some common PBRP stuff.
+$(call inherit-product, vendor/pb/config/common.mk)
 
 PRODUCT_NAME := omni_tiare
