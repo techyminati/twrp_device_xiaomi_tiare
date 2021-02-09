bd0a2769761dea92604009c5f6e18df2fdacf96b
Aryan Sinha <sinha.aryan03@gmail.com>
2021-02-09 21:19:18 +0530

    Fix

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -18,7 +18,7 @@ LOCAL_PATH := device/xiaomi/tiare
 
 # Architecture
 TARGET_ARCH := arm
-TARGET_ARCH_VARIANT := armv7-a-neon
+TARGET_ARCH_VARIANT := armv8-a
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
 TARGET_CPU_VARIANT := cortex-a53
